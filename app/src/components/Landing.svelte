b3272ba8be65665874991fe4751f928849803959
Jonathan Dunne <jonathan@flexdapps.com>
2019-08-24 20:05:33 +1000

    svelte components ya'll

---
+++ app/src/components/Landing.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { onMount } from 'svelte';
+  import { fade, slide, crossfade } from 'svelte/transition'
+</script>
+
+<style>
+</style>
+
+<div class="flex flex-column w-100 h-100" in:fade>
+
+  <div class="f5 pa3">
+    sudz skuds spuds yeet
+  </div>
+
+  <div
+    class="w-100 flex-dapps flex items-center justify-center h5" >
+    
+  </div>
+  
+</div>

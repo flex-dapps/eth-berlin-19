670087220ddbc7e975ec95c13cb9c6245476301f
Tom Nash <contacttomnash@gmail.com>
2019-08-24 16:35:58 +0200

    fuck u ethereum yeet

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -5,22 +5,9 @@
   import { Landing, Home } from "./components";
 
   onMount(async () => {
-    if (window.ethereum) {
-      window.web3 = new Web3(ethereum);
-      try {
-        await ethereum.enable();
-      } catch (error) {
-        // User denied account access...
-        console.error(error);
-      }
-    } else if (window.web3) {
-      window.web3 = new Web3(web3.currentProvider);
-    }
-    await window.ethereum.enable();
     wallet.init(
       "0x1Cea940cA15a303A0E01B7F8589F39fF34308DB2", // tornado
-      "0xc8f51a8ade617c4930f558b19b9491d525d01f13", // proxy
-      window.web3
+      "0xc8f51a8ade617c4930f558b19b9491d525d01f13" // proxy
     );
   });
 

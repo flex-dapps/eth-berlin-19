7df58eaca806e14bc96fc6e885acf87a45f9ab27
Jonathan Dunne <jonathan@flexdapps.com>
2019-08-25 01:53:30 +1000

    MyLoads page

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -2,9 +2,11 @@
   import { Router, Route, navigate } from "svelte-routing";
   import { onMount } from "svelte";
 
-  import { Landing, Home } from "./components";
-<<<<<<< HEAD
-=======
+	import {
+			Landing, 
+			Home, 
+			MyLoads
+		} from './components';
 
   onMount(async () => {
     if (window.ethereum) {
@@ -33,8 +35,11 @@
   }
   function claimBounty() {
     wallet.claimBounty();
-  }
->>>>>>> e4c2f257a3839c25638c3ac4a20eed8920a47e31
+	}
+	
+		
+
+
 </script>
 
 <style>
@@ -48,29 +53,21 @@
     rel="stylesheet"
     href="https://unpkg.com/tachyons@4.10.0/css/tachyons.min.css" />
 </svelte:head>
-
-<div id="app" class="flex h-100 flex-column justify-start relative">
-  <div id="screen" class="h-100 w-100 overflow-hidden">
-    <Router>
-<<<<<<< HEAD
-      <Route>
-        <Landing />
-      </Route>
-      <Route path="/home">
-        <Home />
-      </Route>
-    </Router>
-  </div>
+<div
+  id="app"
+  class="flex h-100 flex-column justify-start relative"
+>
+	<div id="screen" class="h-100 w-100 overflow-hidden" >
+		<Router>
+			<Route path='home'>
+				<Home/>
+			</Route>
+			<Route path='myloads'>
+				<MyLoads />
+			</Route>
+			<Route>
+				<Landing />
+			</Route>
+		</Router>
+	</div>
 </div>
-=======
-      <Route path="home">
-        <Home />
-      </Route>
-      <Route>
-        <Landing />
-      </Route>
-    </Router>
-  </div>
-</div>
->>>>>>> 1b0c494b945ce37f069da4a3b42a89a7400372f0
->>>>>>> e4c2f257a3839c25638c3ac4a20eed8920a47e31
